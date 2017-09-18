ec6fa7690853290a0f538d308580ae65a4ec7117
YaroBear <berejnoi.yaroslav@gmail.com>
2017-09-18 20:48:44 +0000

    Pair programmed some unit tests and minimal code to pass them: expose cell, seal cell, unseal cell, create grid array, create mined cell, distribute mines
    git-svn-id: https://agile.cs.uh.edu/svn/f17_softwaredesign/berejnoi_biddle@595 5f3e6de7-e054-8643-865b-9ca9e3633ffd

--- assign1/test/minesweeper-test.js
+++ assign1/test/minesweeper-test.js
@@ -1,5 +1,46 @@
+var expect = require('chai').expect;
+var Cell = require('../src/Cell.js');
+var Grid = require('../src/Grid.js');
+
 describe('MineSweeper Tests', function() {
-  it('canary test', function() {
-    expect(true).to.be.true;
-  });
+	it('canary test', function() {
+		expect(true).to.be.true;
+	});
+
+	it('should seal a cell', function(){
+		var newCell = new Cell.Cell();
+		newCell.seal();
+		expect(newCell.sealed).to.eql(true);
+	});
+
+
+	it('should unseal a cell', function(){
+		var newCell = new Cell.Cell();
+		newCell.unseal();
+		expect(newCell.sealed).to.eql(false);
+	});
+
+	it('should create an array of cells', function(){
+		var newGrid = new Grid(10, 10, 5);
+		expect(newGrid.grid.length).to.eql(100);
+	});
+
+	it('should make a new MinedCell', function(){
+		var newCell = new Cell.MinedCell();
+		expect(newCell.mined).to.eql(true);
+	});
+	
+	it('should distribute 5 mines', function(){
+		var newGrid = new Grid(10, 10, 5);
+		var count = 0;
+
+		newGrid.distributeMines();
+		for(var i = 0; i < 10*10; i++){
+			if (newGrid.grid[i] instanceof Cell.MinedCell) {
+				count++;
+			}
+		}
+		expect(count).to.eql(5);
+	});
+
 });
