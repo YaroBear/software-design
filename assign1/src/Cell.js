8e9b8a1e9e8b2d400f91338f2420c1a678d8d319
YaroBear <berejnoi.yaroslav@gmail.com>
2017-09-19 01:22:42 +0000

    Accidentally unchecked the files in /src when committing.
    git-svn-id: https://agile.cs.uh.edu/svn/f17_softwaredesign/berejnoi_biddle@604 5f3e6de7-e054-8643-865b-9ca9e3633ffd

---
+++ assign1/src/Cell.js
@@ -0,0 +1,28 @@
+var Cell = function(){
+	this.sealed = false;
+	this.exposed = false;
+	this.mined = false;
+
+};
+
+Cell.prototype.expose = function(){
+	this.exposed = true;
+};
+
+Cell.prototype.seal = function(){
+	this.sealed = true;
+};
+
+Cell.prototype.unseal = function(){
+	this.sealed = false;
+};
+
+
+var MinedCell = function(){
+	Cell.call(this);
+	this.mined = true;
+};
+
+
+module.exports.Cell = Cell;
+module.exports.MinedCell = MinedCell;
---
+++ assign1/src/Grid.js
@@ -0,0 +1,21 @@
+var Cell = require('../src/Cell.js');
+
+var Grid = function(w, h, numberMines){
+	this.width = w;
+	this.height = h;
+	this.numberMines = numberMines;
+	this.grid = [];
+
+	for(var i=0; i < this.width*this.height; i++) {
+		this.grid.push(new Cell.Cell());
+	}
+};
+
+Grid.prototype.distributeMines = function(){
+	for(var i = 0; i < this.numberMines; i++) {
+		var randPos = Math.floor(Math.random() * this.width*this.height) + 1;
+		this.grid[randPos] = new Cell.MinedCell();
+	}
+};
+
+module.exports = Grid;
