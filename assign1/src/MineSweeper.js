b703ed260ad083cb9fb0993858da7950e915cdf6
Nick Biddle <redacted>
2017-09-20 16:25:34 +0000

    Added exception tests for exposing a cell out of row range and exposing a cell out of column range.  Set fixed height and width attributes in the minesweeper class.  The exposeCell() function now throws an error if the row position is greater than the number of rows and a different error if the column position is greater than the number of columns.
    git-svn-id: https://agile.cs.uh.edu/svn/f17_softwaredesign/berejnoi_biddle@729 5f3e6de7-e054-8643-865b-9ca9e3633ffd

--- assign1/src/MineSweeper.js
+++ assign1/src/MineSweeper.js
@@ -1,8 +1,14 @@
 var MineSweeper = function(){
+	this.height = 5;
+	this.width = 5;
 };
 
 MineSweeper.prototype.exposeCell = function(row, column){
-	return true;
+	if (row>=this.height)
+		throw new Error('Out of row range');
+	if(column>=this.width)
+		throw new Error('Out of column range');
+	return 'exposed';
 };
 
 MineSweeper.prototype.isCellExposed = function(row, column){
--- assign1/test/minesweeper-test.js
+++ assign1/test/minesweeper-test.js
@@ -29,4 +29,18 @@ describe('MineSweeper Tests', function() {
 		
 		expect(minesweeper.isCellExposed(1, 2)).to.eql(true);
 	});
+
+	it('should throw an exception when trying to expose a cell out of row range', function(){
+
+		var toCall = function() { minesweeper.exposeCell(7, 2); }
+
+		expect(toCall).to.throw("Out of row range");
+	});
+
+	it('should throw an exception when trying to expose a cell out of column range', function(){
+
+		var toCall = function() { minesweeper.exposeCell(1, 8); }
+
+		expect(toCall).to.throw("Out of column range");
+	});
 });
