4bdca5bf4127ae2141d6e7bdc494fd9bcd966439
YaroBear <berejnoi.yaroslav@gmail.com>
2017-09-20 14:21:50 +0000

    Deleted comments. Put minesweeper instance in beforeEach() loop
    git-svn-id: https://agile.cs.uh.edu/svn/f17_softwaredesign/berejnoi_biddle@721 5f3e6de7-e054-8643-865b-9ca9e3633ffd

--- assign1/src/MineSweeper.js
+++ assign1/src/MineSweeper.js
@@ -1,12 +1,10 @@
 var MineSweeper = function(){
-	//this.Cell = {EXPOSED : false};  //Venkat: not needed yet
 };
 
 MineSweeper.prototype.exposeCell = function(row, column){
-	//this.Cell.EXPOSED = true; //Venkat: not needed yet
+	return true;
 };
 
 MineSweeper.prototype.isCellExposed = function(row, column){
-	//return this.Cell.EXPOSED;  //Venkat: not needed yet
 	return true;
 };
--- assign1/test/minesweeper-test.js
+++ assign1/test/minesweeper-test.js
@@ -3,54 +3,30 @@ describe('MineSweeper Tests', function() {
 		expect(true).to.be.true;
 	});
 
-//Venkat: Please remove review notes before asking for the next review.
-//Venkat: Let's delete these tests and start over with something much simpler.
-//here are a few tests to write at first.
+	var minesweeper;
+	beforeEach(function(){
+		minesweeper = new MineSweeper();
+	});
 
-// expose a cell
-// expose another cell
-// expose an exposed cell
-
-//For the expose a cell, call exposeCell(1, 2) and verify that 
-//isCellExposed(1, 2) is true.
-
-//For expose  an exposed cell test, call exposeCell twice on the same location 
-//and let the reult still be true for isCellExposed. That is calling expose on 
-//an exposed cell is OK.
-
-//Let's do all this test on a MineSweeper class (not Grid, not Cell). At this 
-//time we do not need a Cell class, so let's remove Grid and Cell. For now, we 
-//need MineSweeper class and three tests in our MineSweeper Tests.
-
-//Keep the code very minium (literally). Do not create any fields in your 
-//class until that field is absolutely needed at this time (and it is not 
-//right now).
-
-
-	//New tests based on feedback above  //Venkat: Please do not leave comments in code. We will evolve code continuously based on feedback.
 	it('expose a cell', function(){
-		var minesweeper = new MineSweeper(); //Venkat: let's move this to before_each setup
 		minesweeper.exposeCell(1, 2);
-//Venkat: Let's put a blank line here		
+		
 		expect(minesweeper.isCellExposed(1, 2)).to.eql(true);
 	});
 
 	it('expose another cell', function(){
-		var minesweeper = new MineSweeper();
-		//expose first cell //Venkat: Please remove this line
 		minesweeper.exposeCell(1, 2);
-		//expose second cell //Venkat: Please remove this line
+
 		minesweeper.exposeCell(2, 3);
-//Venkat: Let's put a blank line here		
+	
 		expect(minesweeper.isCellExposed(2, 3)).to.eql(true);
 	});
 
 	it('expose an exposed cell', function(){
-		var minesweeper = new MineSweeper();
-		//expose same cell twice //Venkat: Please remove this line
 		minesweeper.exposeCell(1, 2);
+		
 		minesweeper.exposeCell(1, 2);
-//Venkat: Let's put a blank line here		
+		
 		expect(minesweeper.isCellExposed(1, 2)).to.eql(true);
 	});
 });
