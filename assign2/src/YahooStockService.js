77ad799c3b7e83886bbba83157ed5354694897a5
YaroBear <berejnoi.yaroslav@gmail.com>
2017-10-07 01:38:20 +0000

    Forgot to track YahooStockService.js
    git-svn-id: https://agile.cs.uh.edu/svn/f17_softwaredesign/berejnoi_biddle@1970 5f3e6de7-e054-8643-865b-9ca9e3633ffd

---
+++ assign2/src/YahooStockService.js
@@ -0,0 +1,19 @@
+var request = require('request-promise-native');
+var StockService = require('../src/StockService');
+
+var YahooStockService = function(){
+	StockService.call(this);
+};
+
+YahooStockService.prototype.getStockPrice = function(symbol){
+	return request(`http://download.finance.yahoo.com/d/quotes.csv?s=${symbol}&f=snbaopl1`)
+		.then(function(res){
+			return res;
+		});
+};
+
+YahooStockService.prototype.convertCSVtoArray = function(csv){
+	return csv.split(',');
+};
+
+module.exports = YahooStockService;
