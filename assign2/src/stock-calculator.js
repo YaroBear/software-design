3f2d058233ac264e48b29d964d989f41a823cf70
Nick Biddle <redacted>
2017-10-08 15:35:31 +0000

    refactored getAssetValueForASymbol so that it returns a new stock rather than modifying the stock it is passed.
    git-svn-id: https://agile.cs.uh.edu/svn/f17_softwaredesign/berejnoi_biddle@2054 5f3e6de7-e054-8643-865b-9ca9e3633ffd

--- assign2/src/stock-calculator.js
+++ assign2/src/stock-calculator.js
@@ -18,24 +18,14 @@ class StockCalculator {
     return stocks.map(stock => this.getAssetValueForASymbol(stock));
   }
                
-//Venkat: Instead of mutating the given stock let's create a new one
   getAssetValueForASymbol(stock) {
     try{
     let stockPrice = this.stockService.getStockPrice(stock.symbol);
-    stock.value = this.calculateNetAssetValue({price: stockPrice, count: stock.count});
-    //Venkat: 
-    //const value = this.calculateNetAssetValue({price: stockPrice, count: stock.count});
-    //return {...stock, value };
+    const value = this.calculateNetAssetValue({price: stockPrice, count: stock.count});
+    return {symbol: stock.symbol, count: stock.count, value: value};
     } catch(error) {
-      if (error.message == 'Invalid stock symbol')
-        stock.error = error.message;
-      if (error.message == 'Failed to retrieve data')
-        stock.error = error.message;
-        
-      //Venkat: entire body of the catch can be replaced with this one line
-      //return {...stock, error; error.message };
+      return {symbol: stock.symbol, count: stock.count, error: error.message};
     }
-    return stock; //Venkat: we can remove this line now
   }
 }
 
