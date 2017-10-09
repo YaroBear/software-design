6db19119043e82db1b3088d3f13104eb4376b0a7
YaroBear <berejnoi.yaroslav@gmail.com>
2017-10-09 00:46:28 +0000

    - Changed yahoo-stock-service to use chai-as-promise module instead of using the default promises. - Wrote tests/src for: throwing an error for an invalid symbol, and throwing an error on network/timeout failure. - Left a not for Venkat in yahoo-stock-service
    git-svn-id: https://agile.cs.uh.edu/svn/f17_softwaredesign/berejnoi_biddle@2072 5f3e6de7-e054-8643-865b-9ca9e3633ffd

--- assign2/src/yahoo-stock-service.js
+++ assign2/src/yahoo-stock-service.js
@@ -2,12 +2,18 @@ const request = require('request-promise-native');
 const StockService = require('../src/stock-service');
 
 class YahooStockService extends StockService {
+  constructor(timeout = 1000){
+    super();
+    this.timeout = timeout;
+  }
 
 	getStockInfo(symbol) {
-	return request(`http://download.finance.yahoo.com/d/quotes.csv?s=${symbol}&f=snbaopl1`)
+	return request(`http://download.finance.yahoo.com/d/quotes.csv?s=${symbol}&f=snbaopl1`, {timeout: this.timeout})
 		.then(function(res){
 			return res;
-		});
+		}).catch(function(){
+      throw new Error("Connection error/timeout");
+    });
 	}
 
 	convertCSVtoArray(csv) {
@@ -22,6 +28,7 @@ class YahooStockService extends StockService {
     return this.getStockInfo(symbol)
       .then(function(res){
         let resArray = that.convertCSVtoArray(res);
+        if(resArray[1] == 'N/A') throw new Error("Invalid stock symbol");
         return parseFloat(resArray[2]);
       });
   }
--- assign2/test/yahoo-stock-service-test.js
+++ assign2/test/yahoo-stock-service-test.js
@@ -1,4 +1,6 @@
 const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 const expect = chai.expect;
 const YahooStockService = require('../src/yahoo-stock-service');
 const StockCalculator = require('../src/stock-calculator');
@@ -11,28 +13,18 @@ describe('yahoo stock service tests:', function(){
 		yahooStockService = new YahooStockService();
 	});
 
-//Venkat: Please see the last four (bottom) presentations at                     
-//https://www.agilelearner.com/?q=promise. We can benefit from chai-as-promised
 	it('should connect to the Yahoo stock service and get a response', function(){
-		let response = false;
-		return yahooStockService.getStockInfo('TSLA')
-			.then(function(res){
-				if (res) response = true;
-				expect(response).to.be.true;
-		});
+		return expect(yahooStockService.getStockInfo('TSLA')).to.be.fulfilled;
 	});
-            
-//Venkat: Here we are interested in teting convertCSVtoArray. We should not call getStockInfo in this test
-	it('should convert the csv from YahooStockService into an array', function(){
-		return yahooStockService.getStockInfo('TSLA')
-			.then(function(res){
-				let resArray = yahooStockService.convertCSVtoArray(res);
-				expect(resArray).to.be.an('array');
-		});
+
+	it('should convert a csv string into an array', function(){
+		let csvString = '"TSLA","Tesla, Inc.",352.50,354.39,353.10,355.33,356.88';
+		let csvArray = yahooStockService.convertCSVtoArray(csvString);
+		expect(csvArray).to.be.an('array');
 	});
 
 	it('should retrieve the correct stock info for TSLA', function(){
-		return yahooStockService.getStockInfo('TSLA')
+		return expect(yahooStockService.getStockInfo('TSLA')).to.be.fulfilled
 			.then(function(res){
 				let resArray = yahooStockService.convertCSVtoArray(res);
 				expect(resArray[1]).to.eql('Tesla, Inc.');
@@ -40,12 +32,24 @@ describe('yahoo stock service tests:', function(){
 	});
 
 	it('should get the stock price for TSLA', function(){
-		return yahooStockService.getStockPrice('TSLA')
+		return expect(yahooStockService.getStockPrice('TSLA')).to.be.fulfilled
 			.then(function(price){
 				expect(typeof price).to.be.eql("number");
 			});
 	});
-});
 
-//Venkat: a test for invalid ticket symbol
-//a test for network failure
+	it('should throw an error for an invalid symbol', function(){
+		return expect(yahooStockService.getStockPrice('ABC123'))
+			.to.be.rejectedWith('Invalid stock symbol');
+	});
+
+	// To Venkat: should we use mock-require to override request.defaults({timeout: 1})
+	// instead of setting the timeout as part of the YahooStockService constructor?
+	// https://github.com/request/request#convenience-methods
+	it('should throw an error for network/timeout failure', function(){
+		let quickTimeYahooService = new YahooStockService(1);
+		return expect(quickTimeYahooService.getStockInfo('GOOG'))
+			.to.be.rejectedWith('Connection error/timeout')
+	});
+	
+});
