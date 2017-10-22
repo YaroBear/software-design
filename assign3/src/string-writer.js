f4ed63e20f1dd606d22882f4dd08aa745f75ffc2
YaroBear <berejnoi.yaroslav@gmail.com>
2017-10-22 16:47:02 +0000

    Removed StringWriter.write() and StringWriter.removeDuplicates() and their associated tests as they violate SRP
    git-svn-id: https://agile.cs.uh.edu/svn/f17_softwaredesign/berejnoi_biddle@2718 5f3e6de7-e054-8643-865b-9ca9e3633ffd

--- assign3/src/string-writer.js
+++ assign3/src/string-writer.js
@@ -8,21 +8,10 @@ class StringWriter{
 		if(this.open){
 			this.contents += string;
 		}
-		else throw new Error('Closed for writing'); //Venkat: no need to blow up
-	}
-
-	read(){ //Venkat: Please remove
-		return this.contents;
 	}
 	
 	close(){
 		this.open = false;
-	}
-
-	removeDuplicates(){ //Venkat: vilates SRP and OCP by being here.
-		this.contents = this.contents.split(" ")
-			.filter((word, index, array) => {if (word != array[index + 1]) return word})
-			.join(" ");
 	}
 }
 
--- assign3/test/string-writer-test.js
+++ assign3/test/string-writer-test.js
@@ -18,50 +18,25 @@ describe('string writer tests:', function(){
 		expect(stringWriter.contents).to.be.eql("Some string");
 	});
 
-	it('should read the content', function(){ //Venkat: Please remove
-		stringWriter.write("Some string");
-		expect(stringWriter.read()).to.be.eql("Some string");
-	});
-
 	it('should write multiple times', function(){
 		stringWriter.write("Some string");
 		stringWriter.write(" yet another one");
 		stringWriter.write(" and one more for good measure");
-		expect(stringWriter.read()).to.be.eql("Some string yet another one and one more for good measure"); //Venkat: stringWriter.contents instead of stringWriter.read()
+		expect(stringWriter.contents).to.be.eql("Some string yet another one and one more for good measure");
 	});
 
-	it('should close contents for writing', function(){ //Venkat: please remove
+	it('should close contents after writing once', function(){
+		stringWriter.write("Some string");
 		stringWriter.close();
-		let call =  function(){stringWriter.write("Some string");};
+		stringWriter.write(" yet another one");
 
-		expect(call).to.throw("Closed for writing");
+		expect(stringWriter.contents).to.be.eql("Some string");
 	});
 
-//Venkat: let's call write, close, and then write. Check the contents contains only stuff from the first write and not the second.
-//Then another test, close, write, check the contents is empty string
+	it('should close contents before writing and leave contents empty', function(){
+		stringWriter.close();
+		stringWriter.write("Some string");
 
-//Venkat: Please remove all tests below this. Violates OCP. If we decide to add another such function, we have to keep changing this class. We want to make this extensible, instead.
-	it('should remove multiple consecutive duplicate words within a string', function(){
-		stringWriter.write("string with with with duplicates");
-
-		stringWriter.removeDuplicates();
-
-		expect(stringWriter.read()).to.be.eql("string with duplicates");
-	});
-
-	it('should remove consecutive duplicate words from the end of a string', function(){
-		stringWriter.write("string with duplicates duplicates");
-
-		stringWriter.removeDuplicates();
-
-		expect(stringWriter.read()).to.be.eql("string with duplicates");
-	});
-
-	it('should only allow one instance of a word that is repeated many times', function(){
-		stringWriter.write("duplicates duplicates duplicates duplicates duplicates");
-
-		stringWriter.removeDuplicates();
-
-		expect(stringWriter.read()).to.be.eql("duplicates");
+		expect(stringWriter.contents).to.be.eql("");
 	});
 });
