f05ed0ca5553987965949d38fea6478ebdf015db
YaroBear <berejnoi.yaroslav@gmail.com>
2017-10-23 23:26:11 +0000

    Removed chai-as-promised for now. Didn't use it while rewriting file-writer tests due to the chained nature
    git-svn-id: https://agile.cs.uh.edu/svn/f17_softwaredesign/berejnoi_biddle@2765 5f3e6de7-e054-8643-865b-9ca9e3633ffd

--- assign3/test/file-writer-test.js
+++ assign3/test/file-writer-test.js
@@ -1,7 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
-const chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
+
 const FileWriter = require('../src/file-writer');
 const fs = require('fs-extra');
 
