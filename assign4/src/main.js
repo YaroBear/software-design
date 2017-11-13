678c288707cd08616745b905c30c04184e368b7c
Professor <redacted>
2017-11-13 15:36:05 +0000

    how to change the order of transformation?
    git-svn-id: https://agile.cs.uh.edu/svn/f17_softwaredesign/berejnoi_biddle@3865 5f3e6de7-e054-8643-865b-9ca9e3633ffd

--- assign4/src/main.js
+++ assign4/src/main.js
@@ -9,7 +9,7 @@ Object.values(avatars).forEach((avatar) =>{
 
 let avatar = Object.values(avatars)[0];
 
-let player = new Player(new avatar, new Transformations(Object.keys(avatars)));
+let player = new Player(new avatar, new Transformations(Object.keys(avatars))); //Venkat: The order in which the files are loaded/required is not necessarily the order of transformation.
 
 for(let i = 0; i < 6; i++){
     player.performAction();
