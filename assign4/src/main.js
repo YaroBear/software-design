c5ae9a322dc398b2b0787c9c7de1701b7f55da3c
YaroBear <berejnoi.yaroslav@gmail.com>
2017-11-15 04:11:39 +0000

    Made main.js take command line arguments instead
    git-svn-id: https://agile.cs.uh.edu/svn/f17_softwaredesign/berejnoi_biddle@3949 5f3e6de7-e054-8643-865b-9ca9e3633ffd

--- assign4/src/main.js
+++ assign4/src/main.js
@@ -1,32 +1,24 @@
 const Player = require('./player');
 const Transformations = require('./transformations');
 
+if (process.argv.length < 4){
+	throw new Error("must have at least two args: npm start bike bike car plane rocket...");
+	process.exit();
+}
+
 let avatars = require('require.all')('./avatars');
 
 Object.values(avatars).forEach((avatar) =>{
     avatar.prototype.action = function(){console.log("Calling action of : " + this.constructor.name);}
 });
 
-let avatar = Object.values(avatars)[0];
+let startingAvatar = new avatars[process.argv[2]];
 
-let defaultRules = Object.keys(avatars);
+let defaultRules = process.argv.slice(3);
 
 let transformations = new Transformations(defaultRules);
 
-let player = new Player(new avatar, transformations);
-
-for(let i = 0; i < 6; i++){
-    player.performAction();
-    player.transformUp();
-}
-
-let newRules = defaultRules.sort(function(a,b) {
-	return 0.5 - Math.random();
-});
-
-transformations = new Transformations(newRules);
-
-player.changeTransformations(transformations);
+let player = new Player(startingAvatar, transformations);
 
 for(let i = 0; i < 6; i++){
     player.performAction();
